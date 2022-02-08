5a361e6e900868add343dde733d2cf741e680ffc
z00590711 <zhongkeyi1@huawei.com>
2022-02-08 16:44:08 +0800

    update

--- hisilicon/sec2/sec.h
+++ hisilicon/sec2/sec.h
@@ -1,69 +1,222 @@
-/* SPDX-License-Identifier: GPL-2.0+ */
-/*
- * Copyright (c) 2018-2019 HiSilicon Limited.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- */
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (c) 2018-2019 HiSilicon Limited. */
 
-#ifndef HISI_SEC_H
-#define HISI_SEC_H
+#ifndef __HISI_SEC_V2_H
+#define __HISI_SEC_V2_H
 
-#include <linux/list.h>
-#include "../qm.h"
-#include "sec_usr_if.h"
+#include "../hisi_acc_qm.h"
+#include "sec_crypto.h"
 
-#undef pr_fmt
-#define pr_fmt(fmt)	"hisi_sec: " fmt
-
-#define CTX_Q_NUM_DEF		24
-#define FUSION_LIMIT_DEF	1
-#define FUSION_LIMIT_MAX	64
-#define FUSION_TMOUT_NSEC_DEF	(400 * 1000)
-
-enum sec_endian {
-	SEC_LE = 0,
-	SEC_32BE,
-	SEC_64BE
+/* Algorithm resource per hardware SEC queue */
+struct sec_alg_res {
+	u8 *pbuf;
+	dma_addr_t pbuf_dma;
+	u8 *c_ivin;
+	dma_addr_t c_ivin_dma;
+	u8 *a_ivin;
+	dma_addr_t a_ivin_dma;
+	u8 *out_mac;
+	dma_addr_t out_mac_dma;
 };
 
-struct hisi_sec_ctrl;
-
-enum hisi_sec_status {
-	HISI_SEC_RESET,
+/* Cipher request of SEC private */
+struct sec_cipher_req {
+	struct hisi_acc_hw_sgl *c_out;
+	dma_addr_t c_out_dma;
+	u8 *c_ivin;
+	dma_addr_t c_ivin_dma;
+	struct skcipher_request *sk_req;
+	u32 c_len;
+	bool encrypt;
 };
 
-struct hisi_sec_dfx {
-	u64 send_cnt;
-	u64 send_by_tmout;
-	u64 send_by_full;
-	u64 recv_cnt;
-	u64 get_task_cnt;
-	u64 put_task_cnt;
-	u64 gran_task_cnt;
-	u64 thread_cnt;
-	u64 fake_busy_cnt;
-	u64 busy_comp_cnt;
-	u64 sec_ctrl;
+struct sec_aead_req {
+	u8 *out_mac;
+	dma_addr_t out_mac_dma;
+	u8 *a_ivin;
+	dma_addr_t a_ivin_dma;
+	struct aead_request *aead_req;
 };
 
-struct hisi_sec {
+struct sec_ahash_req {
+	struct scatterlist *req_sg;
+	u8 sg_cut_len[SEC_MAX_SG_OF_REMAIN];
+	u32 cut_num;
+	u8 op;
+	u8 done;
+	struct ahash_request *ahash_req;
+	/* Means long hash and ping-pong buffer flag */
+	bool is_stream_mode;
+	u32 req_data_len;
+	/* Currently ping-pong data len */
+	u32 pp_data_len;
+	u64 total_data_len;
+	/* Length of data every bd sent to hardware */
+	u32 block_data_len;
+	int sid;
+
+	struct scatterlist *pp_sg;
+	/* Current pingpong hw sg dma address */
+	dma_addr_t pp_dma;
+};
+
+/* SEC request of Crypto */
+struct sec_req {
+	union {
+		struct sec_sqe sec_sqe;
+		struct sec_sqe3 sec_sqe3;
+	};
+	struct sec_ctx *ctx;
+	struct sec_qp_ctx *qp_ctx;
+
+	/**
+	 * Common parameter of the SEC request.
+	 * ahash, hardware sgl used to hold request's source buffers
+	 */
+	struct hisi_acc_hw_sgl *in;
+	dma_addr_t in_dma;
+	struct sec_cipher_req c_req;
+	struct sec_aead_req aead_req;
+	struct sec_ahash_req hash_req;
+	struct list_head backlog_head;
+
+	int err_type;
+	int req_id;
+	u32 flag;
+
+	/* Status of the SEC request */
+	bool fake_busy;
+	bool use_pbuf;
+};
+
+struct sec_req_op {
+	int (*buf_map)(struct sec_ctx *ctx, struyct sec_req *req);
+	void (*buf_unmap)(struct sec_ctx *ctx, struyct sec_req *req);
+	void (*do_transfer)(struct sec_ctx *ctx, struyct sec_req *req);
+	int (*bf_fill)(struct sec_ctx *ctx, struyct sec_req *req);
+	int (*bd_send)(struct sec_ctx *ctx, struyct sec_req *req);
+	void (*callback)(struct sec_ctx *ctx, struyct sec_req *req, int err);
+	int (*process)(struct sec_ctx *ctx, struyct sec_req *req);
+}
+
+struct sec_auth_ctx {
+	dma_addr_t a_key_dma;
+	u8 *a_key;
+	u8 a_key_len;
+	u8 mac_len;
+	u8 a_alg;
+	u32 blk_size;
+	u32 align_sz;
+	dma_addr_t metamac_dma;
+	void *metamac;
+	bool fallback;
+
+	char metamac_idx[SEC_MAX_STREAMS];
+	struct crypto_shash *hash_tfm;
+	struct crypto_shash *fallback_ahash_tfm;
+	struct shash_desc *desc;
+	struct crypto_shash *fallback_aead_tfm;
+};
+
+struct sec_cipher_ctx {
+	u8 *c_key;
+	dma_addr_t c_key_dma;
+	sector_t iv_offset;
+	u32 c_gran_size;
+	u32 ivsize;
+	u8 c_mode;
+	u8 c_alg;
+	u8 c_key_len;
+
+	bool fallback;
+	struct crypto_sync_skcipher *fbtfm;
+};
+
+struct sec_qp_ctx {
+	struct hisi_qp *qp;
+	struct sec_req *req_list[QM_Q_DEPTH];
+	struct idr req_idr;
+	struct sec_alg_res res[QM_Q_DEPTH];
+	struct sec_ctx *ctx;
+	struct mutex req_lock;
+	struct list_head backlog;
+	struct hisi_acc_sgl_pool *c_in_pool;
+	struct hisi_acc_sgl_pool *c_out_pool;
+};
+
+enum sec_alg_type {
+	SEC_SKCIPHER,
+	SEC_AEAD,
+	SEC_AHASH
+};
+
+struct pending_sgl {
+	struct scatterlist sgl[MERGE_SGL_NUM];
+	u32 len;
+};
+
+struct sec_ctx {
+	struct sec_qp_ctx *qp_ctx;
+	struct sec_dev *sec;
+	const struct sec_req_op *req_op;
+	struct hisi_qp **qps;
+
+	u32 hlf_q_num;
+
+	u32 fake_req_limit;
+
+	atomic_t enc_qcyclic;
+
+	atomic_t dec_qcyclic;
+
+	enum sec_alg_type alg_type;
+	bool pbuf_supported;
+	struct sec_cipher_ctx c_ctx;
+	struct sec_auth_ctx a_ctx;
+	u8 type_supported;
+
+	struct pending_sgl pingpong_sg[SEC_MAX_STREAMS][PINGPONG_BUF_NUM];
+	u8 pingpong_idx[SEC_MAX_STREAMS];
+	struct idr stream_idr;
+	struct mutex stream_idr_lock;
+	struct device *dev;
+};
+
+enum sec_debug_file_index {
+	SEC_CLEAR_ENABLE,
+	SEC_DEBUG_FILE_NUM,
+};
+
+struct sec_debug_file {
+	enum sec_debug_file_index index;
+	spinlock_t lock;
+	struct hisi_qm *qm;
+};
+
+struct sec_dfx {
+	atomic64_t send_cnt;
+	atomic64_t recv_cnt;
+	atomic64_t send_busy_cnt;
+	atomic64_t recv_busy_cnt;
+	atomic64_t err_bd_cnt;
+	atomic64_t invalid_req_cnt;
+	atomic64_t done_flag_cnt;
+};
+
+struct sec_debug {
+	struct sec_dfx dfx;
+	struct dfx_info info[MAX_DFX_FILES_NUM];
+	struct sec_debug_file files[SEC_DEBUG_FILE_NUM];
+};
+
+struct sec_dev {
 	struct hisi_qm qm;
-	struct list_head list;
-	struct hisi_sec_dfx sec_dfx;
-	struct hisi_sec_ctrl *ctrl;
-	struct mutex *hisi_sec_list_lock;
-	int q_ref;
-	int ctx_q_num;
-	int fusion_limit;
-	int fusion_tmout_nsec;
-	unsigned long status;
+	struct sec_debug debug;
+	u32 ctx_q_num;
 };
 
-struct hisi_sec *find_sec_device(int node);
-int hisi_sec_register_to_crypto(void);
-void hisi_sec_unregister_from_crypto(void);
+void sec_destroy_qps(struct hisi_qp **qps, int qp_num);
+struct hisi_sec **sec_create_qps(void);
+int sec_register_to_crypto(struct hisi_qm *qm);
+void sec_unregister_from_crypto(struct hisi_qm *qm);
 #endif
--- hisilicon/sec2/sec_crypto.h
+++ hisilicon/sec2/sec_crypto.h
@@ -1,21 +1,246 @@
-/* SPDX-License-Identifier: GPL-2.0+ */
-/*
- * Copyright (c) 2018-2019 HiSilicon Limited.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- */
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (c) 2018-2019 HiSilicon Limited. */
 
-#ifndef HISI_SEC_CRYPTO_H
-#define HISI_SEC_CRYPTO_H
+#ifndef __HISI_SEC_V2_CRYPTO_H
+#define __HISI_SEC_V2_CRYPTO_H
 
+#define SEC_AIV_SIZE 12
 #define SEC_IV_SIZE 24
 #define SEC_MAX_KEY_SIZE 64
+#define SEC_COMM_SCENE  0
+#define SEC_IPSEC_SCENE 1
+#define SEC_STREAM_SCENE    0X7
+#define SEC_MAX_STREAMS 128
+#define SEC_SHA1_ALIGN_SZ   64
+#define SEC_MAX_SG_OF_REMAIN    8
+#define PINGPONG_BUF_NUM    2
+#define SEC_MIN_BLOCK_SZ    1
+#define MERGE_SGL_NUM       2
 
-int hisi_sec_register_to_crypto(void);
-void hisi_sec_unregister_from_crypto(void);
+enum sec_calg {
+    SEC_CALG_3DES = 0x1;
+    SEC_CALG_AES  = 0x2;
+    SEC_CALG_SM4  = 0x3;
+};
+
+enum sec_hash_alg {
+    SEC_A_HMAC_SHA1     = 0x10,
+    SEC_A_HMAC_SHA256   = 0x11,
+    SEC_A_HMAC_MD5      = 0x12,
+    SEC_A_HMAC_SHA512   = 0x15,
+    SEC_A_HMAC_SM3      = 0x26,
+};
+
+enum sec_mac_len {
+    SEC_HMAC_CCM_MAC    = 16,
+    SEC_HMAC_GCM_MAC    = 16,
+    SEC_SM3_MAC         = 32,
+    SEC_HMAC_SM3_MAC    = 32,
+    SEC_HMAC_MD5_MAC    = 16,
+    SEC_HMAC_SHA1_MAC   = 20,
+    SEC_HMAC_SHA256_MAC = 32,
+    SEC_HMAC_SHA512_MAC = 64,
+};
+
+enum sec_cmode {
+    SEC_CMODE_ECB   = 0x0,
+    SEC_CMODE_CBC   = 0x1,
+    SEC_CMODE_CFB   = 0x2,
+    SEC_CMODE_OFB   = 0x3,
+    SEC_CMODE_CTR   = 0x4,
+    SEC_CMODE_CCM   = 0x5,
+    SEC_CMODE_GCM   = 0x6,
+    SEC_CMODE_XTS   = 0x7,
+};
+
+enum sec_ckey_type {
+    SEC_CKEY_128BIT = 0x0,
+    SEC_CKEY_192BIT = 0x1,
+    SEC_CKEY_256BIT = 0x2,
+    SEC_CKEY_3DES_3KEY = 0x1,
+    SEC_CKEY_3DES_2KEY = 0x3,
+};
+
+enum sec_bd_type {
+    SEC_BD_TYPE1 = 0x1,
+    SEC_BD_TYPE2 = 0x2,
+    SEC_BD_TYPE3 = 0x3,
+};
+
+enum sec_auth {
+    SEC_NO_AUTH = 0x0,
+    SEC_AUTH_TYPE1 = 0x1,
+    SEC_AUTH_TYPE2 = 0x2,
+};
+
+enum sec_cipher_dir {
+    SEC_CIPHER_ENC = 0x1,
+    SEC_CIPHER_DEC = 0x2,
+};
+
+enum sec_addr_type {
+    SEC_PBUF = 0x0,
+    SEC_SGL  = 0x1,
+    SEC_PRP  = 0x2,
+};
+
+struct bd_status {
+    u64 tag;
+    u8 done;
+    u8 err_type;
+    u16 flag;
+    u16 icv;
+};
+
+enum {
+    AUTHPAD_PAD,
+    AUTHPAD_NOPAD,
+};
+
+
+struct sec_sqe_type2 {
+	__le32 mac_key_alg;
+    __le16 icvw_kmode;
+    __u8 c_alg;
+    __u8 rsvd4;
+    __le32 alen_ivllen;
+    __le32 clen_ivllen;
+    __le16 auth_src_offset;
+    __le16 cipher_src_offset;
+    __le16 cs_ip_header_offset;
+    __le16 cs_udp_header_offset;
+    __le16 pass_word_len;
+    __le16 dk_len;
+    __u8 salt3;
+    __u8 salt2;
+    __u8 salt1;
+    __u8 salt0;
+
+    __le16 tag;
+    __le16 rsvd5;
+    __le16 cph_pad;
+    __le16 c_pad_len_field;
+
+    __le64 long_a_data_len;
+    __le64 a_ivin_addr;
+    __le64 a_key_addr;
+    __le64 mac_addr;
+    __le64 c_ivin_addr;
+    __le64 c_key_addr;
+    __le64 data_src_addr;
+    __le64 data_dst_addr;
+
+    __le16 done_flag;
+
+    __u8 error_type;
+    __u8 warning_type;
+    __u8 mac_i3;
+    __u8 mac_i2;
+    __u8 mac_i1;
+    __u8 mac_i0;
+    __le16 check_sum_i;
+    __u8 tls_pad_len_i;
+    __u8 rsvd12;
+    __le32 counter;
+};
+
+struct sec_sqe {
+	__u8 type_cipher_auth;
+	__u8 sds_sa_type;
+	__u8 sdm_addr_type;
+	__u8 rsvd0;
+	__u8 huk_key_ci;
+	__u8 ai_apd_cs;
+	__u8 rca_key_frm;
+	__u8 iv_tls_ld;
+	struct sec_sqe_type2 type2;
+};
+
+struct bd3_auth_ivin {
+    __le64 a_ivin_addr;
+    __le32 rsvd0;
+    __le32 rsvd1;
+} __packed __aligned(4);
+
+struct bd3_skip_data {
+    __le32 rsvd0;
+    __le32 gran_num;
+    __le32 src_skip_data_len;
+    __le32 dst_skip_data_len;
+};
+
+struct bd3_stream_scene {
+    __le64 c_ivin_addr;
+    __le64 long_a_data_len;
+    __u8 stream_auth_pad;
+    __u8 plaintext_type;
+    __le16 pad_len_1p3;
+} __packed __aligned(4);
+
+struct bd3_no_scene {
+    __le64 c_ivin_addr;
+    __le32 rsvd0;
+    __le32 rsvd1;
+    __le32 rsvd2;
+} __packed __aligned(4);
+
+struct bd3_check_sum {
+    __u8 rsvd0;
+    __u8 hac_sva_status;
+    __le16 check_sum_i;
+};
+
+struct bd3_tls_type_back {
+    __u8 tls_1p3_type_back;
+    __u8 hac_sva_status;
+    __le16 pad_len_1p3_back;
+};
+
+struct sec_sqe3 {
+    __le32 bd_param;
+    __le16 c_icv_key;
+    __u8 c_mode_alg;
+    __u8 huk_iv_seq;
+
+    __le64 tag;
+    __le64 data_src_addr;
+    __le64 a_key_addr;
+
+    union{
+        struct bd3_auth_ivin auth_ivin;
+        struct bd3_skip_data skip_data;
+    };
+
+    __le64 c_key_addr;
+    __le32 auth_mac_key;
+    __le32 salt;
+    __le16 auth_src_offset;
+    __le16 cipher_src_offset;
+    __le32 a_len_key;
+    __le32 clen_ivin;
+    __le64 data_dst_addr;
+    __le64 mac_addr;
+    union {
+        struct bd3_stream_scene stream_scene;
+        struct bd3_no_scene no_scene;
+    };
+
+    __le16 done_flag;
+    __u8 error_type;
+    __u8 warning_type;
+    union {
+        __le32 mac_i;
+        __le32 kek_key_addr_l;
+    };
+    union {
+        __le32 kek_key_addr_h;
+        struct bd3_check_sum check_sum;
+        struct bd3_tls_type_back tls_type_back;
+    };
+    __le32 counter;
+} __packed __aligned(4);
+
+int sec_register_to_crypto(struct hisi_qm *qm);
+void sec_unregister_from_crypto(struct hisi_qm *qm);
 
 #endif
